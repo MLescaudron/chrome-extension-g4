8ae8bec737a034913dac89f5fe2ea03d16511475
mathieu <dev@lescaudron.com>
2017-09-15 11:57:17 +1200

    Update G4 extension, work on g4-education

--- scripts/g4-stop.js
+++ scripts/g4-stop.js
@@ -1,31 +1,34 @@
-// End
 $('#jqxButtonNext').parent().append('<button type="button" style="border-color:#af8484 !important;background:linear-gradient(to bottom,#d44343 0,#961717 100%) !important" class="btn btn-primary" id="endCourse" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Contenu suivant">Terminer le cours maintenant ! <span class="zmdi zmdi-skip-next zmdi-hc-lg"></span></button>');
 $('#jqxButtonNext').css('display', 'none');
 
 $('body').on('click', '#endCourse', function () {
-	let time = getRandomInt(120, 600); // 7 min
-	if ($('.bandeaucondicourssuivi button') && $('.bandeaucondicourssuivi button').text()) {
-		time = $('.bandeaucondicourssuivi button').text().split(' ');
-		if (time && time.length && time.length > 4) {
-			time = parseInt(time[4]);
-		}
-		console.log(time);
-		if (!time || time <= 0 || typeof time == "undefined") {
-			time = getRandomInt(120, 600); // 7 min
-		} else {
-			time = time * 60;
-			time = getRandomInt(time, time + 420)
-		}
-	}
-	compteurtemps(500000, 70000);
-	setTimeout(function () {
-		compteurtemps(time, 70000);
-	}, 200);
-	setTimeout(function () {
-		$('#jqxButtonNext').click();
-	}, 400);
+  $('#jqxButtonNext button').html("Let's go !");
+  var time = getRandomInt(4, 7); // between 4 and 7 min
+  if ($('.bandeaucondicourssuivi button') && $('.bandeaucondicourssuivi button').text()) {
+    time = $('.bandeaucondicourssuivi button').text().split(' ');
+    if (time && time.length && time.length > 4) {
+      time = parseInt(time[ 4 ]);
+    }
+    if (!time || time <= 0 || typeof time == "undefined") {
+      // usefull for non video courses
+      time = getRandomInt(4, 7); // between 4 and 7 min
+    }
+  }
+  $('#endCourse').html("Course ended in " + finishHim(time) + 'min, ...');
+  setTimeout(function () {
+    $('#jqxButtonNext').click();
+  }, 2000);
 });
 
 function getRandomInt(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function finishHim(timeInMin) {
+  // Yeah I put var because of browser compatibilities
+  var timeInSec = (timeInMin * 60) + getRandomInt(1, 60);
+  var timeDivide15 = Math.round(timeInSec / 15);
+  // finish course
+  compteurtemps(timeInSec, timeDivide15);
+  return (timeInSec / 60).toFixed(2);
 }
--- scripts/g4.js
+++ scripts/g4.js
@@ -2,35 +2,31 @@
  * Get the data send.
  */
 chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        var data = request.data;
-        if (!data) return;
-        switch (data.type) {
-            case 'endCourse':
-                endCourse();
-                break;
-            default:
-        }
-        sendResponse('Work in Progress...');
+  function (request, sender, sendResponse) {
+    var data = request.data;
+    if (!data) return;
+    switch (data.type) {
+      case 'endCourse':
+        endCourse();
+        break;
+      default:
     }
+    sendResponse('Work in Progress...');
+  }
 );
-
-// Get data in the url easily
-function getParameterByName(name, url) {
-
-}
-endCourse();
+setTimeout(function(){
+  endCourse();
+},2000);
 // End course in the page
 function endCourse() {
-    var s = document.createElement('script');
-// TODO: add "script.js" to web_accessible_resources in manifest.json
-    s.src = chrome.extension.getURL('scripts/g4-stop.js');
-    console.log(s.src);
-    s.onload = function() {
-        this.remove();
-    };
-    (document.head || document.documentElement).appendChild(s);
+  var s = document.createElement('script');
+  s.src = chrome.extension.getURL('scripts/g4-stop.js');
+  console.log(s.src);
+  s.onload = function () {
+    this.remove();
+  };
+  (document.head || document.documentElement).appendChild(s);
 
-    // Say to the extension, it's done
-    chrome.runtime.sendMessage({text: 'end', data: {}});
+  // Say to the extension, it's done
+  chrome.runtime.sendMessage({ text: 'end', data: {} });
 }
